144b3ded2d6a8d474f1dd61d551c94a2abaa2ebd
aman <amanuel.assefa@xcome.com>
2020-06-28 14:17:17 +0800

    config Mapping for open api

--- app/index.js
+++ app/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
-
+const axios = require('axios');
 // Constants
 const PORT = 8000;
 const HOST = '0.0.0.0';
 
 // App
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
 const app = express();
+
+const OWM_API_KEY = process.env.OWM_API_KEY || 'invalid_key';
+const UNITS = process.env.UNITS || 'metric';
 app.get('/', (req, res) => {
-  res.send('Hello World version 3');
+  axios.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=' + OWM_API_KEY)
+  .then(response => {
+    console.log(response.data.url);
+    res.send(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
 });
 
 app.listen(PORT, HOST);
